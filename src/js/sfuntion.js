b1f3a444a14ec5798718ac4894b1af968936ded4
biennt <biennt@dfm-engineering.com>
2019-12-10 16:57:56 +0700

    Show image in modal

--- src/js/sfuntion.js
+++ src/js/sfuntion.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
     clockCountdown();
     changeImageSource();
-
+    imageModalClick();
 });
 
-function clockCountdown(){
+function clockCountdown() {
     var end = $('#select-countdown').val();
     var now = new Date();
     var yearNow = now.getFullYear();
     var yearNext = yearNow + 1;
 
-    nowTime = (Date.parse(now) / 1000);	
+    nowTime = (Date.parse(now) / 1000);
     endThisYear = end.replace("year", yearNow);
     endTime = (Date.parse(endThisYear) / 1000);
 
     var timeLeft = endTime - nowTime;
-    if(timeLeft < 0){
+    if (timeLeft < 0) {
         endNextYear = end.replace("year", yearNext);
         endTime = (Date.parse(endNextYear) / 1000);
         timeLeft = endTime - nowTime;
     }
 
-    var days = Math.floor(timeLeft / 86400); 
+    var days = Math.floor(timeLeft / 86400);
     var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-    var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+    var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
     var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
 
     if (hours < "10") { hours = "0" + hours; }
@@ -33,23 +33,39 @@ function clockCountdown(){
     $("#days-countdown").html(days + "<span>Days</span>");
     $("#hours-countdown").html(hours + "<span>Hours</span>");
     $("#minutes-countdown").html(minutes + "<span>Minutes</span>");
-    $("#seconds-countdown").html(seconds + "<span>Seconds</span>");		
+    $("#seconds-countdown").html(seconds + "<span>Seconds</span>");
 
-	setInterval(function() { clockCountdown(); }, 1000);
+    setInterval(function() { clockCountdown(); }, 1000);
 }
 
-function changeImageSource(){
-    $('#select-countdown').change(function(){
-        var end = $('#select-countdown').val();console.log(end.indexOf('04 October'));
-        if(end.indexOf('04 October') != -1){
+function changeImageSource() {
+    $('#select-countdown').change(function() {
+        var end = $('#select-countdown').val();
+        console.log(end.indexOf('04 October'));
+        if (end.indexOf('04 October') != -1) {
             $('#hours-countdown').bind().on('click', function() {
                 var images = $('.data-carousel-3d').find('img');
-                images.each(function(){
-                    var src = $(this).attr('src');console.log(src);
-                    src = src.replace('image', 'loveImage');console.log(src);
+                images.each(function() {
+                    var src = $(this).attr('src');
+                    console.log(src);
+                    src = src.replace('image', 'loveImage');
+                    console.log(src);
                     $(this).attr('src', src);
                 })
             });
         }
     });
+}
+
+function imageModalClick() {
+    $('img').bind('click').click(function() {
+        var src = $(this).attr('src');
+        $('#imageModal').css('display', 'block');
+        $('#imageBuildIn').attr('src', src);
+        console.log(src);
+    })
+
+    $('.btn-close').click(function() {
+        $('#imageModal').css('display', 'none');
+    })
 }
